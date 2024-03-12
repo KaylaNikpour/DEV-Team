68e3e919dae7321eab6416e387af3b3111f2fd32
KaylaNikpour <kayla.nikpour@edu.sait.ca>
2024-03-12 00:17:24 -0600

    Docs: Fixed the responsivness of the javascript for the navigation bar.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
-
+// For nav bar + hamburger
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
+  function resize() {
+    let x = document.getElementById("myLinks");
+      x.style.display = "flex";
+        if (window.outerWidth < 800) {
+    x.style.display = "none"; }
+        else {x.style.display = "flex"; }
+  }
+// Copyright
+  document.getElementById("year").innerHTML = new Date().getFullYear();
