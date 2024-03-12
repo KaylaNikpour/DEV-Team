253e64abf5a8c540dc82041b4aa279979c8b7da7
KaylaNikpour <kayla.nikpour@edu.sait.ca>
2024-03-11 22:50:30 -0600

    Docs: Got hamburger java and styling rendered.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
